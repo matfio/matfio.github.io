529f6c1daf13c17b71a449a55642ce56252d6c8f
matfio <mtt.fiorio@gmail.com>
2022-04-11 16:45:45 +0200

    setup git pages

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Layout from './pages/Layout';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout />
   </React.StrictMode>,
   document.getElementById('root')
 );
